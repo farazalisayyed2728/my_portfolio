4aec72b17513c7960054c9391081e5dcc1854ba2
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-08-09 20:30:54 +0530

    add data

--- portapp/views.py
+++ portapp/views.py
@@ -5,3 +5,25 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'portapp/home.html')
+
+
+def about(request):
+    return render(request, 'portapp/about.html')
+
+
+def skills(request):
+    return render(request, 'portapp/skills.html')
+
+
+def projects(request):
+    return render(request, 'portapp/projects.html')
+
+
+def contact(request):
+    return render(request, 'portapp/contact.html')
